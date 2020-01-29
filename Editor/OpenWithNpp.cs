756105c4e2e23c0a47a8d7f4a0c3e2ac41a885e4
david-sigaty <david@sigaty.dev>
2020-01-29 09:36:59 -0700

    Initial commit.

---
+++ Editor/OpenWithNpp.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using TeamCitrus.Editor;
+
+public class OpenWithNpp : OpenWithMenuItem
+{
+	public const string Name = "Notepad++";
+	public const string Key = BaseKey + Name;
+	public const string DefaultPath = "notepad++";
+	private const string MenuItemPath = BasePath + Name;
+	private const string EditMenuItemPath = EditPath + Name;
+	
+	[MenuItem(MenuItemPath, false, BasePriority)]
+	public static void Open() { Execute<OpenWithNpp>(); }
+
+	[MenuItem(MenuItemPath, true)]
+	public static bool OpenValidation() { return Validate<OpenWithNpp>(); }
+	
+	[MenuItem(EditMenuItemPath, false, 1)]
+	public static void EditOpen() { Execute<OpenWithNpp>(); }
+
+	[MenuItem(EditMenuItemPath, true)]
+	public static bool EditOpenValidation() { return Validate<OpenWithNpp>(); }
+}
